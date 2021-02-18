cab84041aca8668e3214f56eb44953266b4a3017
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2021-02-18 18:27:38 +0100

    Changes in home. Schemas imported

--- ClientSW/src/app/home/home.module.ts
+++ ClientSW/src/app/home/home.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
-  ]
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeModule { }
