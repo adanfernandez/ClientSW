73cd4df2e9f0ab2d13248326028ab323fcbf4d9d
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2021-02-28 04:56:30 +0100

    Panel creation

--- ClientSW/src/app/model/panel.ts
+++ ClientSW/src/app/model/panel.ts
@@ -3,7 +3,7 @@ import { State } from "./state";
 export interface Panel {
     id?: number;
     name?: string;
-    id_user?: number;
+    userId?: number;
     states?: State[];
     new?: boolean;
 }
--- ClientSW/src/app/panel/create-panel/create-panel.component.ts
+++ ClientSW/src/app/panel/create-panel/create-panel.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Panel } from 'src/app/model/panel';
+import { State } from 'src/app/model/state';
+import { PanelService } from '../../services/panel.service';
+import { StateService } from '../../services/state.service';
 
 @Component({
   selector: 'app-create-panel',
@@ -7,27 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePanelComponent implements OnInit {
 
-  constructor() { }
+  constructor(public panelService: PanelService, public stateService: StateService) { }
 
-  states: String[] = [];
+  states: string[] = [];
+
+  title: string = "";
 
   stateToAdd: String = "";
 
   ngOnInit(): void {
-    this.states.push("Estado 1");
-    this.states.push("Estado 2");
-    this.states.push("Estado 3");
-    this.states.push("Estado 4");
   }
 
-  public addState() {
+  /*public addState() {
     if(this.stateToAdd) {
       this.states.push(this.stateToAdd);
     }
     this.stateToAdd = "";
-  }
+  }*/
 
   public removeState(i: number) {
     this.states.splice(i,1);
+  }
+
+  savePanel() {
+    const id = localStorage.getItem("id");
+    const panel: Panel = {
+      userId: Number(id),
+      name: this.title,
+    }
+    this.panelService.savePanel(panel).subscribe();
+      /*res =>{
+        this.states.map((state: string) => {
+          const stateAdd: State = {
+            name: state,
+
+          }
+          this.stateService.saveState(stateAdd).subscribe();
+        })
+      }
+      
+    );*/
   }
 }
